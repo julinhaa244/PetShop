401b659de0c7596ca13949e0aaa419d421edfc31
julinhaa244 <163298923+julinhaa244@users.noreply.github.com>
2025-02-26 07:42:24 -0300

    Add files via upload

---
+++ CadastroDeAnimais/src/br/com/petshop/Gato.java
@@ -0,0 +1,20 @@
+package br.com.petshop;
+
+public class Gato extends Animal {
+	private String corPelo;
+	public Gato(String nome, int idade, String corPelo) {
+		super(nome, idade);
+		this.corPelo = corPelo;
+	}
+	public void miar() {
+		System.out.println(getNome()+"esta miando: miauu miauu");
+}
+	public void exibirInfo() {
+		super.exibirInfo();
+		System.out.println("cor do pelo:" + corPelo);
+		
+		
+		
+	}
+
+}
---
+++ CadastroDeAnimais/src/br/com/petshop/PetShop.java
@@ -0,0 +1,10 @@
+package br.com.petshop;
+
+public class PetShop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
